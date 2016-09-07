191925e301853c8f4cb2b82b94e15f375919c69b
Administrator <detective.ive@163.com>
2016-09-07 16:33:57 +0800

    learn from http://code.tutsplus.com/tutorials/getting-started-with-reactivex-on-android--cms-24387

--- app/src/main/java/com/ssdy/mainpercenttest/MainActivity.java
+++ app/src/main/java/com/ssdy/mainpercenttest/MainActivity.java
@@ -6,11 +6,18 @@ import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.List;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Subscription;
+import rx.functions.Action;
+import rx.functions.Action1;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,22 +25,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private ListView mList;
 
+    Observer<String> _Observer;
+    Observable<String> _Observable;
+
     /**
      * Called with the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Inflate our UI from its XML layout description.
         setContentView(R.layout.activity_main);
 
-        // Get display items for later interaction
         Button speakButton = (Button) findViewById(R.id.ibtn_study);
-
         mList = (ListView) findViewById(R.id.ibtn_analysis);
-
-        // Check to see if a recognition activity is present
         PackageManager pm = getPackageManager();
         List<ResolveInfo> activities = pm.queryIntentActivities(
                 new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
@@ -43,11 +47,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             speakButton.setEnabled(false);
             speakButton.setText("Recognizer not present");
         }
+
+        _Observer = new Observer<String>() {
+            @Override
+            public void onCompleted() {
+                Log.d("OBSERVER_", "onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d("OBSERVER_", "onError");
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d("OBSERVER_onNext", s);
+            }
+        };
+        _Observable = Observable.just("Hello"); // Emits "Hello"
+        Subscription _Subscription = _Observable.subscribe(_Observer);//这个操作会经过onNext和onCompleted
+        /*As these methods are often left unused, you also have the option of
+        using the Action1 interface, which contains a single method named call.
+        意思是，如果你根本不用到onError和onCompleted，可以直接使用以下这个返单个call的方法
+         */
+        Action1<String> _Action1 = new Action1<String>() {
+            @Override
+            public void call(String s) {
+                Log.d("OBSERVER_Action",s);
+            }
+        };
+        _Subscription = _Observable.subscribe(_Action1);
+        /*To detach an observer from its observable while the observable is
+        still emitting data, you can call the unsubscribe method on the Subscription object.
+        解除绑定（原意是把在观察observable的observer分离），如果被观察着还在不断的发消息，可以用订阅者的unsubscribe方法取消继续发送
+         */
+        _Subscription.unsubscribe();
     }
 
-    /**
-     * Handle the click on the start recognition button.
-     */
     public void onClick(View v) {
         if (v.getId() == R.id.ibtn_study) {
 
